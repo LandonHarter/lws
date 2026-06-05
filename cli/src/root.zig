06face77e8b54a7a51b6644a531cd52a147f426e
LandonHarter <landonharter@outlook.com>
2026-06-05 16:31:21 -0400

    cli init

---
+++ cli/src/root.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const zli = @import("zli");
+
+const version = @import("commands/version.zig");
+
+pub fn build(init_options: zli.InitOptions) !*zli.Command {
+    const root = try zli.Command.init(init_options, .{
+        .name = "LWS CLI",
+        .description = "LWS command line interface",
+        .version = .{ .major = 0, .minor = 0, .patch = 1, .pre = null, .build = null },
+    }, showHelp);
+
+    try root.addCommands(&.{
+        try version.register(init_options),
+    });
+
+    return root;
+}
+
+fn showHelp(ctx: zli.CommandContext) !void {
+    try ctx.command.printHelp();
+}
